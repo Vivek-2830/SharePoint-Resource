3b3e6d2ec14c345acba74d51b417a932dcc5460d
Vivek-2830 <gohilvivek0303@gmail.com>
2024-12-02 17:21:23 +0530

    Update a Template Design

--- src/webparts/sharepointDesign/components/SharepointDesign.tsx
+++ src/webparts/sharepointDesign/components/SharepointDesign.tsx
@@ -36,6 +36,7 @@ export default class SharepointDesign extends React.Component<ISharepointDesignP
           <div className="ms-Grid">
             <div className="ms-Grid-row">
               <div className="ms-Grid-col">
+
                 <div className='content'>
                   <div className="hero">
                     <div className="hero-content">
@@ -70,8 +71,34 @@ export default class SharepointDesign extends React.Component<ISharepointDesignP
                         })
                     }
                   </div>  
-                </div>
 
+                  {/* <div className="container">
+                    <div className="card">
+                      <div className="imgBx">
+                        <img src="https://assets.codepen.io/4164355/shoes.png" />
+                      </div>
+                      <div className="contentBx">
+                          <h2>Nike Shoes</h2>
+                          <div className="size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                          </div>
+                          <div className="color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                          </div>
+                          <a href="#">Buy Now</a>
+                      </div>
+                    </div>
+                  </div> */}
+
+
+                </div>
               </div>
             </div>
           </div>
